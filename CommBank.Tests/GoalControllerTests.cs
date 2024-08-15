811dc8ef0c8148ab7d6ca53bf76ad288012bffbe
yby <yuby0404_qd@163.com>
2024-08-15 20:30:48 +1000

    add Icon and Finish getforuser test

--- CommBank.Tests/GoalControllerTests.cs
+++ CommBank.Tests/GoalControllerTests.cs
@@ -66,9 +66,23 @@ public class GoalControllerTests
     public async void GetForUser()
     {
         // Arrange
-        
+        var goals = collections.GetGoals();
+        var users = collections.GetUsers();
+        IGoalsService goalsService = new FakeGoalsService(goals, goals[0]);
+        IUsersService usersService = new FakeUsersService(users, users[0]);
+        GoalController controller = new(goalsService, usersService);
+
         // Act
-        
-        // Assert
+        var httpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext();
+        controller.ControllerContext.HttpContext = httpContext;
+        var result = await controller.GetForUser(goals[0].UserId!);
+        Assert.NotNull(result);
+        var index = 0;
+        foreach (Goal goal in result!)
+        {
+            Assert.IsAssignableFrom<Goal>(goal);
+            Assert.Equal(goals[0].UserId, goal.UserId);
+            index++;
+        }
     }
 }
